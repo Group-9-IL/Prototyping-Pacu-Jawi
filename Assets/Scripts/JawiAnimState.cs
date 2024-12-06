e55d846e08fcf380583e2c18c893ea75dbfcc477
brannn86 <gibran.hattami@gmail.com>
2024-12-06 21:07:40 +0700

    feat: jawi animation completed set: run, reverse, sprint. with JawiAnimState.cs controller. additional utilities script included: ReverseClip.cs for reversing anims clips without hassle

--- Assets/Scripts/JawiAnimState.cs
+++ Assets/Scripts/JawiAnimState.cs
@@ -4,17 +4,27 @@ public class CharacterAnimation : MonoBehaviour
 {
     Animator animator;
     int isRunningHash;
+    int isReversingHash;
+    int isSprintingHash;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         isRunningHash = Animator.StringToHash("isRunning");
+        isReversingHash = Animator.StringToHash("isReversing");
+        isSprintingHash = Animator.StringToHash("isSprinting");
     }
 
     void Update()
     {
         bool isRunning = animator.GetBool(isRunningHash);
+        bool isReversing = animator.GetBool(isReversingHash);
+        bool isSprinting = animator.GetBool(isSprintingHash);
+
         bool forwardPressed = Input.GetKey("w");
+        bool reversePressed = Input.GetKey("s");
+        bool sprintPressed = Input.GetKey("left shift");
+
         if (!isRunning && forwardPressed) // Example: press W to run
         {
             animator.SetBool(isRunningHash, true);
@@ -22,6 +32,24 @@ public class CharacterAnimation : MonoBehaviour
         if (isRunning && !forwardPressed)
         {
             animator.SetBool(isRunningHash, false);
+        }
+
+        if (!isReversing && reversePressed)
+        {
+            animator.SetBool(isReversingHash, true);
+        }
+        if (isReversing && !reversePressed)
+        {
+            animator.SetBool(isReversingHash, false);
+        }
+
+        if (!isSprinting && (forwardPressed && sprintPressed))
+        {
+            animator.SetBool(isSprintingHash, true);
+        }
+        if (isSprinting && (!forwardPressed || !sprintPressed))
+        {
+            animator.SetBool(isSprintingHash, false);
         }
     }
 }
